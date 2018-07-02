a69720de3026e7bb2d858a1c3cac93ebada7332a
S. Wan <Mycroft0121@gmail.com>
2018-07-02 15:24:06 -0400

    implement doubly linked list for lineage

--- lineage_tracking.py
+++ lineage_tracking.py
@@ -14,10 +14,10 @@ class Cell(object):
         self.length       = length       # cell length
         self.intensities  = intensities  # reporters intensity list
 
-    def give_birth(self, next, birth_time):
-        self.next = next
-        # update the pole_tracker
-        self.pole_tracker[0] += 1
+    # def give_birth(self, next, birth_time):
+    #     self.next = next
+    #     # update the pole_tracker
+    #     self.pole_tracker[0] += 1
 
     def get_lost(self, lost_time):
         self.lost_time = lost_time
@@ -30,49 +30,57 @@ class Lineage(object):
         self.birth_time = birth_time
         self.size = 0
         self.mother = mother
-        self.daughter = None
+        self.daughters = None    # will be a list of lineage objects
 
-        def append(self, frame_ID, birth_time):
-            if self.head is None:
-                new_cell = Cell(frame_ID,birth_time)
-                self.head = new_cell
-            else:
-                new_cell = Cell(frame_ID, birth_time, prev=self.tail)
-                self.tail = new_cell
-                self.tail.next = new_cell
-            self.size += 1
+    def append(self, frame_ID, birth_time):
+        if self.head is None:
+            new_cell = Cell(frame_ID,birth_time)
+            self.head = new_cell
+        else:
+            new_cell = Cell(frame_ID, birth_time, prev=self.tail)
+            self.tail = new_cell
+            self.tail.next = new_cell
+        self.size += 1
 
-        def remove(self,frame_ID):
-            cur_cell = self.head
-            while cur_cell is not None:
-                if cur_cell.frame_ID == frame_ID:
-                    if cur_cell.prev is not None:
-                        cur_cell.prev.next = cur_cell.next
-                        cur_cell.next.prev =  cur_cell.prev
-                    else:
-                        self.head = cur_cell.next
-                        cur_cell.next.prev = None
-                cur_cell = cur_cell.next
+    def remove(self,frame_ID):
+        cur_cell = self.head
+        while cur_cell is not None:
+            if cur_cell.frame_ID == frame_ID:
+                if cur_cell.prev is not None:
+                    cur_cell.prev.next = cur_cell.next
+                    cur_cell.next.prev =  cur_cell.prev
+                else:
+                    self.head = cur_cell.next
+                    cur_cell.next.prev = None
+            cur_cell = cur_cell.next
 
-        def insert_after(self, inserted, prev_ID):
-            cur_cell = self.head
-            while cur_cell is not None:
-                if cur_cell.frame_ID == prev_ID:
-                    inserted.prev = cur_cell
-                    inserted.next = cur_cell.next
-                    cur_cell.next = inserted
-                    return
-                cur_cell = cur_cell.next
-
-        def insert_after(self, inserted, prev_ID):
-            cur_cell = self.head
-            while cur_cell is not None:
-                if cur_cell.frame_ID == prev_ID:
-                    inserted.prev = cur_cell
-                    inserted.next = cur_cell.next
-                    cur_cell.next = inserted
-                    return
-                cur_cell = cur_cell.next
+    # def insert_after(self, inserted, prev_ID):
+    #     cur_cell = self.head
+    #     while cur_cell is not None:
+    #         if cur_cell.frame_ID == prev_ID:
+    #             inserted.prev = cur_cell
+    #             inserted.next = cur_cell.next
+    #             cur_cell.next = inserted
+    #
+    #             return
+    #         cur_cell = cur_cell.next
+    #     print('insertion failed')
+    #     return 1
+    #
+    # def insert_before(self, inserted, next_ID):
+    #     cur_cell = self.head
+    #     while cur_cell is not None:
+    #         if cur_cell.frame_ID == next_ID:
+    #             if cur_cell.prev is not None:
+    #                 cur_cell.prev.next = inserted
+    #                 cur_
+    #
+    #
+    #                 inserted.prev = cur_cell
+    #                 inserted.next = cur_cell.next
+    #                 cur_cell.next = inserted
+    #                 return
+    #         cur_cell = cur_cell.next
 
 
 
