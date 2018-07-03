af21b44df81754453c5bc193259d06efc55475ff
S. Wan <Mycroft0121@gmail.com>
2018-07-03 17:58:47 -0400

    algo outline

--- lineage_tracking.py
+++ lineage_tracking.py
@@ -4,8 +4,8 @@
 class Cell(object):
     def __init__(self, frame_ID, birth_time, length, intensities, prev=None, next=None,  mother=None, daughter=None, lost_time=None, pole_tracker=None):
         self.frame_ID     = frame_ID     # (t, #), use to retrieve cell properties
-        self.prev         = prev         # the same cell in the previous frame, recorded as Cell
-        self.next         = next         # the same cell in the next frame, recorded as Cell
+        # self.prev         = prev         # the same cell in the previous frame, recorded as Cell
+        # self.next         = next         # the same cell in the next frame, recorded as Cell
         # self.birth_time   = birth_time   # t, when was this cell born
         # self.mother       = mother       # frame ID of its mother
         # self.daughter     = daughter     # frame ID of its daughter
@@ -42,48 +42,32 @@ class Lineage(object):
             self.tail.next = new_cell
         self.size += 1
 
-    def remove(self,frame_ID):
+    def remove(self, frame_ID):
         cur_cell = self.head
         while cur_cell is not None:
             if cur_cell.frame_ID == frame_ID:
                 if cur_cell.prev is not None:
                     cur_cell.prev.next = cur_cell.next
-                    cur_cell.next.prev =  cur_cell.prev
+                    cur_cell.next.prev = cur_cell.prev
                 else:
                     self.head = cur_cell.next
                     cur_cell.next.prev = None
+                self.size -= 1
+                return
             cur_cell = cur_cell.next
+        print('removal error')
+        return 1
 
-    # def insert_after(self, inserted, prev_ID):
-    #     cur_cell = self.head
-    #     while cur_cell is not None:
-    #         if cur_cell.frame_ID == prev_ID:
-    #             inserted.prev = cur_cell
-    #             inserted.next = cur_cell.next
-    #             cur_cell.next = inserted
-    #
-    #             return
-    #         cur_cell = cur_cell.next
-    #     print('insertion failed')
-    #     return 1
-    #
-    # def insert_before(self, inserted, next_ID):
-    #     cur_cell = self.head
-    #     while cur_cell is not None:
-    #         if cur_cell.frame_ID == next_ID:
-    #             if cur_cell.prev is not None:
-    #                 cur_cell.prev.next = inserted
-    #                 cur_
-    #
-    #
-    #                 inserted.prev = cur_cell
-    #                 inserted.next = cur_cell.next
-    #                 cur_cell.next = inserted
-    #                 return
-    #         cur_cell = cur_cell.next
+    def give_birth(self, frame_ID):
+        return
 
 
-
+# algorithms to identify lineage
+# input data structure: frame_IDs for a single trench
+#
+# 1. starting from the mother, create a lineage, track it along time
+# 2. once a division event happened, initiate a new lineage with the daughter
+# 3. repeat the tracking backwards from the youngest daughter to the oldest
 
 
 
